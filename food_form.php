2846eaa9ccb7138ae2881081c4942c3a13818ca6
adjoamaison <Agatha Maison>
2015-04-13 03:02:15 +0000

    adding to meal table complete

--- food_form.php
+++ food_form.php
@@ -47,10 +47,54 @@
 				$ven = $_REQUEST['vendor'];
 				$type = $_REQUEST['type'];
 
-				// include_once ("food_class.php");
-				$obj = new food();
 				
-				$obj ->add_food($name,$desc,$price,$ven,$type);
+				$target_dir = "assets\images\\";
+				$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+				$uploadOk = 1;
+				$img="";
+				$imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
+				// Check if image file is a actual image or fake image
+				if(isset($_POST["submit"])) {
+				    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+				    if($check !== false) {
+				        echo "File is an image - " . $check["mime"] . ".";
+				        $uploadOk = 1;
+				    } else {
+				        echo "File is not an image.";
+				        $uploadOk = 0;
+				    }
+				}
+				// Check if file already exists
+				if (file_exists($target_file)) {
+				    echo "Sorry, file already exists.";
+				    $uploadOk = 0;
+				}
+				// Check file size
+				if ($_FILES["fileToUpload"]["size"] > 500000) {
+				    echo "Sorry, your file is too large.";
+				    $uploadOk = 0;
+				}
+				// Allow certain file formats
+				if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+				&& $imageFileType != "gif" ) {
+				    echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+				    $uploadOk = 0;
+				}
+				// Check if $uploadOk is set to 0 by an error
+				if ($uploadOk == 0) {
+				    echo "Sorry, your file was not uploaded.";
+				// if everything is ok, try to upload file
+				} else {
+				    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+				    	$img = basename( $_FILES["fileToUpload"]["name"]);
+				        echo "The file ".$img." has been uploaded.";
+				    } else {
+				        echo "Sorry, there was an error uploading your file.";
+				    }
+				}
+
+				$obj = new food();				
+				$obj ->add_food($name,$desc,$price,$ven,$type, $img);
 			}
 		?>
 		<!--Import jQuery before materialize.js-->
