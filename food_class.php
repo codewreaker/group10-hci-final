f0011d20cac877717c69c41b581efac34dd19bef
adjoamaison <Agatha Maison>
2015-04-13 01:42:42 +0000

    second commit

--- food_class.php
+++ food_class.php
@@ -5,5 +5,12 @@
 			$str_query="select vendor from c_credentials";
 			return $this->query($str_query);
 		}
+
+		function add_food($name,$desc,$price,$ven,$type){
+			$str_query="insert into c_meals set meal_name='$name', meal_desc='$desc',
+				meal_price='$price', meal_type='$type', vendor='$ven'";
+				echo "$str_query";
+			return $this->query($str_query);
+		}
 	}
 ?>
